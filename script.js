3a3af1133f7cd144079459c2b9afd4d284af370e
Timhojin <ihavealreadylost@gmail.com.>
2024-08-08 11:53:46 +1000

    Added break; for evey case in switch.
    A bug occurs when there is no break;

--- script.js
+++ script.js
@@ -20,31 +20,44 @@ function playRound(humanChoice, computerChoice) {
         switch(computerChoice) {
             case "rock":
                 k = 0;
+                break;
             case "paper":
                 k = -1;
+                break;
             case "scissors":
                 k = 1;
+                break;
         }
     }
-    if(humanChoice === "paper") {
+    else if(humanChoice === "paper") {
         switch(computerChoice) {
             case "rock":
                 k = 1;
+                break;
             case "paper":
                 k = 0;
+                break;
             case "scissors":
                 k = -1;
+                break;
         }
     }
-    if(humanChoice === "scissors") {
+    else if(humanChoice === "scissors") {
         switch(computerChoice) {
             case "rock":
                 k = -1;
+                break;
             case "paper":
                 k = 1;
+                break;
             case "scissors":
                 k = 0;
+                break;
         }
+    }
+    else {
+        k = -1;
+        console.log("INVALID input. The computer wins.");
     }
 
     if(k === 1) {
@@ -63,9 +76,9 @@ function playGame() {
         playRound(getHumanChoice(), getComputerChoice());
 
     console.log(' ');
-    if(humanScore > computerScore) console.log(`You are WINNER! You won by ${humanScore} to ${computerScore}.`);
-    else if(humanScore < computerScore) console.log(`You Lost. The computer won by ${computerScore} to ${humanScore}.`);
-    else console.log(`DRAW. The game tied by ${humanScore} to ${computerScore}.`);
+    if(humanScore > computerScore) console.log(`You are WINNER! You won ${humanScore}-${computerScore}.`);
+    else if(humanScore < computerScore) console.log(`You Lost. The computer won ${humanScore}-${computerScore}.`);
+    else console.log(`DRAW. The game ended in a ${humanScore}-${computerScore} tie.`);
 }
 
 playGame();
